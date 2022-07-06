e002f2985b3088ca13a9a7a9835d4ffe950f8888
MiraLAL <3917kd@yahoo.ne.jp>
2022-07-06 12:00:41 +0900

    String(文字描画)のファイルを追加

---
+++ クイズランリーグ/Source/String.cpp
@@ -0,0 +1,8 @@
+#include "String.h"
+
+float String(float pos_x, float pos_y, const unsigned char string, float size)
+{
+
+
+	return 0.0f;
+}
---
+++ クイズランリーグ/Source/String.h
@@ -0,0 +1,3 @@
+#pragma once
+
+float String(float pos_x, float pos_y, const unsigned char string, float size);
